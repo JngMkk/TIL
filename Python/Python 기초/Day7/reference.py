266810e708b073db9844a053d130ac883e5a81cb
JngMkk <kjm0817ss@gmail.com>
2022-01-03 18:21:10 +0900

    Python

--- Python/Python 기초/Day7/reference.py
+++ Python/Python 기초/Day7/reference.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('c:\\Users\\JngMK\\Desktop\\TIL\\Python\\Python 기초\\Day8')
+sys.path.append('c:\\Users\\[]\\Desktop\\TIL\\Python\\Python 기초\\Day8')
 
 from package1 import module1, module2
 module1.func1()
