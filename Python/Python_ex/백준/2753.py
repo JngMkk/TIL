24e91e4632991cd6d2d51b136584a3991c206b1f
JngMkk <87686562+JngMkk@users.noreply.github.com>
2022-01-08 03:23:33 +0900

    Delete Python/Python_ex/백준 directory

--- Python/Python_ex/백준/2753.py
+++
@@ -1,9 +0,0 @@
-# https://www.acmicpc.net/problem/2753
-
-import sys
-input = sys.stdin.readline
-yr = int(input())
-if yr % 4 == 0 and (yr % 100 != 0 or yr % 400 == 0):
-    print(1)
-else:
-    print(0)
--- Python/Python_ex/백준/2884.py
+++
@@ -1,8 +0,0 @@
-# https://www.acmicpc.net/problem/2884
-
-import sys
-input = sys.stdin.readline
-h, m = map(int, input().split())
-m -= 45
-if m < 0: h -= 1
-print(h%24, m%60)
