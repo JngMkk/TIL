82ae00fc05f62060ebce0e3c9c319feea893626e
JngMkk <kjm0817ss@gmail.com>
2022-01-10 11:49:27 +0900

    자료구조

--- Python/자료구조/04_Queue & Deque/5_CircularDeque.py
+++ Python/자료구조/04_Queue & Deque/5_CircularDeque.py
@@ -7,19 +7,6 @@
     > 전단과 후단에서 모두 삽입, 삭제가 가능한 큐
 
 """
-# 파이썬 제공 queue 모듈 사용
-import queue
-
-# 테스트
-Q = queue.Queue(maxsize = 20)
-for i in range(1, 10):
-    Q.put(i)                        # 삽입
-print('큐의 내용 : ', end = '')
-for _ in range(1, 10):
-    print(Q.get(), end = ' ')       # 삭제
-print()
-
-
 # 원형 큐를 이용한 구현
 # 원형 큐에서 추가되는 연산 : delete_rear(), add_front(), get_rear()    전단, 후단 삽입, 삭제 가능해야 하므로
 from CircularQueue import CircularQueue
