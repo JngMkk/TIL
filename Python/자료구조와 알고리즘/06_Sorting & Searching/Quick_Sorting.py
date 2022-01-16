dde12b6c3e39f55d59b4f1b125c87364205aa7cb
JngMkk <kjm0817ss@gmail.com>
2022-01-17 01:16:54 +0900

    DS&ALGO

---
+++ Python/자료구조와 알고리즘/06_Sorting & Searching/Quick_Sorting.py
@@ -0,0 +1,35 @@
+"""
+퀵 정렬
+
+- 기준 데이터를 설정하고 그 기준보다 큰 데이터와 작은 데이터의 위치를 바꾸는 방법
+- 일반적인 상황에서 가장 많이 사용되는 정렬 알고리즘 중 하나
+- 병합 정렬과 더불어 대부분의 프로그래밍 언어의 정렬 라이브러리의 근간이 되는 알고리즘
+- 가장 기본적인 퀵 정렬은 첫 번째 데이터를 기준 데이터(Pivot)로 설정
+
+- 시간복잡도
+    . 평균의 경우 : O(NlogN)
+    . 최악의 경우 : O(n^2)
+
+"""
+
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while left <= right:
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+        while right > start and array[right] >= array[pivot]:
+            right -= 1
+        if left > right:
+            array[right], array[pivot] = array[pivot], array[right]
+        else:
+            array[left], array[right] = array[right], array[left]
+    quick_sort(array, start, right-1)
+    quick_sort(array, right+1, end)
+
+arr = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+quick_sort(arr, 0, len(arr) - 1)
+print(arr)
