440723877ab576e7dcef2631907d0e8cd4676fc9
JngMkk <kjm0817ss@gmail.com>
2022-02-03 17:55:57 +0900

    Django

---
+++ Django/hello/hello01/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# SSR (Server Side Rendering) / CSR (Client Side Rendering)
+def index(request):
+    return HttpResponse("<h1><a href='/hello01/test'>Hello, Django</h1>")
