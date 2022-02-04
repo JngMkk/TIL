b7ca9a5f9d678fe22791a1f63208a28eaa2d8268
JngMkk <kjm0817ss@gmail.com>
2022-02-04 11:29:36 +0900

    make var app

--- Django/tags/var/urls.py
+++ Django/tags/var/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index),
-    path('var/', include('var.urls')),
+    path('var01/', views.variables01),
 ]
