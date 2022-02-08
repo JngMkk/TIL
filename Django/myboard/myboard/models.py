617ea94abad1e347a59bba996f045328e89f6194
JngMkk <kjm0817ss@gmail.com>
2022-02-08 11:05:38 +0900

    add model

--- Django/myboard/myboard/models.py
+++ Django/myboard/myboard/models.py
@@ -11,3 +11,14 @@ class MyBoard(models.Model):
                     'mytitle':self.mytitle,
                     'mycontent':self.mycontent,
                     'mydate':self.mydate})
+
+class MyMember(models.Model):
+    myname = models.CharField(max_length=100)
+    mypw = models.CharField(max_length=100)
+    myemail = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str({'myname': self.myname,
+                    'mypw': self.mypw,
+                    'myemail': self.myemail
+                    })
