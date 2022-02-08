9aaa4b506a27e83ec97a4ca55f0809d07347154d
JngMkk <kjm0817ss@gmail.com>
2022-02-08 12:05:58 +0900

    go

---
+++ Go/Hello.go
@@ -0,0 +1,7 @@
+package main // 패키지 선언
+
+import "fmt" // 패키지 불러오기 format
+
+func main() {
+	fmt.Println("Hello world!")
+}
---
+++ Go/variables.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	/* var 변수명 자료형 */
+	// overflow
+	// var num uint8 = 28000
+	var num2 uint8 = 28
+	num2 = num2 + 100
+	println(num2)
+}
