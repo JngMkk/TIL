ea1ac1c742ef33fe4cdd888c090f13661a16e97b
JngMkk <kjm0817ss@gmail.com>
2022-02-09 17:08:07 +0900

    insta upd

--- Python/Crawling/Insta/instalike.py
+++ Python/Crawling/Insta/instalike.py
@@ -8,19 +8,19 @@ driver.get('https://www.instagram.com')
 
 time.sleep(2)
 
-email = '01071370240'
+email = input("id : ")
 input_id = driver.find_elements_by_css_selector('input._2hvTZ.pexuQ.zyHYP')[0]
 input_id.clear()
 input_id.send_keys(email)
 
-password = 'wndahek12@'
+password = input("pw : ")
 input_pw = driver.find_elements_by_css_selector('input._2hvTZ.pexuQ.zyHYP')[1]
 input_pw.clear()
 input_pw.send_keys(password)
 input_pw.submit()
 time.sleep(3)
 
-name = 'rlaehrma'
+name = input("Search id : ")
 input_search = driver.find_element_by_css_selector('input.XTCLo.x3qfX')
 input_search.clear()
 input_search.send_keys(name)
